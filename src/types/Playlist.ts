1049c7275f51fef9ab2d557f19f911ab820d7882
Daniel <76593188+Rany300@users.noreply.github.com>
2023-04-15 14:10:02 +0200

    +Code clean up

--- src/types/Playlist.ts
+++ src/types/Playlist.ts
@@ -1,14 +1,14 @@
-import { Title } from "./Title"; 
+import { Title } from "./Title";
 
 export type PlaylistWithoutId = Omit<Playlist, "id">;
 
 export type Playlist = {
-    id: string;
-    gradient: string;
-    name: string;
-    titles: Title[];
-    isFavorites?: boolean;
-    featured?: boolean;
-    cover?: React.ReactNode;
-    type: "personal" | "top";
-}
+  id: string;
+  gradient: string;
+  name: string;
+  titles: Title[];
+  isFavorites?: boolean;
+  featured?: boolean;
+  cover?: React.ReactNode;
+  type: "personal" | "top";
+};
